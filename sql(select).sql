a986c3c02c516a9d128d2f2bf20218e0e2eaf0b8
Lubynets Kirill <mr.lubinets@gmail.com>
2021-12-23 07:29:24 +0200

    sql команды на базе ключевого слова SELECT

---
+++ sql(select).sql
@@ -0,0 +1,19 @@
+#Работаем с SQL 27 стр
+#Выборка данных
+#1
+-- SELECT ALL  onum,  amt, odata
+-- FROM orders;
+#2
+-- SELECT ALL *
+-- FROM customers
+-- WHERE snum = 1001;
+#3
+-- SELECT city, sname, snum,comm
+-- FROM sales_people;
+#4
+-- SELECT rating, cname
+-- FROM customers
+-- WHERE city = 'San Jose';
+#5
+-- SELECT DISTINCT snum
+-- FROM orders;
